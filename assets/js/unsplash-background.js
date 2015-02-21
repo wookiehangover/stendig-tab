04bd5fab87877abe9a3dbae56d2748d417b3ee8a
Sam Breed <sam@quickleft.com>
2015-02-20 22:21:24 -0800

    switching back to request because superagent bugs

--- assets/js/unsplash-background.js
+++ assets/js/unsplash-background.js
@@ -1,41 +1,44 @@
-var request = require('superagent');
+var request = require('request');
 var imagesLoaded = require('imagesloaded');
 
 function appendImage(src) {
   var img = new Image();
   img.src = src;
-  img.className = 'bg fade-in';
+  img.className = 'bg';
   imagesLoaded(img).on('done', function() {
     document.body.appendChild(img);
+    img.className = 'bg fade-in';
 
     setTimeout(function() {
       img.classList.remove('fade-in');
-    }, 0);
+    }, 1000);
   });
 }
 
 module.exports = function () {
-  request
-    .get('//unsplash-api.herokuapp.com/')
-    .end(function(res) {
-      var body = res.body;
-      if (!(body && body.unsplash) ) {
-        // oh well
-        return;
-      }
+  request({
+    method: 'get',
+    url: 'https://unsplash-api.herokuapp.com/',
+    json: true,
+    withCredentials: false
+  }, function(err, resp, body) {
+    if (err || !(body && body.unsplash) ) {
+      // oh well
+      return;
+    }
 
-      var index = Math.floor(Math.random() * body.unsplash.length);
-      var entry = body.unsplash[index];
+    var index = Math.floor(Math.random() * body.unsplash.length);
+    var entry = body.unsplash[index];
 
-      if (entry &&
-          entry.image &&
-          entry.image[0] &&
-          entry.image[0].url) {
+    if (entry &&
+        entry.image &&
+        entry.image[0] &&
+        entry.image[0].url) {
 
-        var url = entry.image[0].url[0];
-        appendImage(url.split('?')[0] + '?w=' + window.outerWidth);
-      }
-    });
-};
+      var url = entry.image[0].url[0];
+      appendImage(url.split('?')[0] + '?w=' + window.outerWidth);
+    }
+  });
+}
 
 
